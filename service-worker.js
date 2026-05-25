77e09df8ab83bd7932a073476b3655106c4335bc
MoolintiVaishnavi <moolintivaishnavi@gmail.com>
2026-05-25 20:21:03 +0530

    All files related to app

---
+++ service-worker.js
@@ -0,0 +1,32 @@
+const CACHE = 'sscb-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  // Always go network-first for Firebase calls
+  if (e.request.url.includes('firebaseio') || e.request.url.includes('googleapis')) {
+    return;
+  }
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
